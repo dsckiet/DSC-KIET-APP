27ba77208d13b94d7b146f6dd3c2d988e536ce33
Ayush783 <ayushc45xxx@gmail.com>
2021-04-26 22:11:33 +0530

    increased icon size and white border added in team

--- lib/screens/team_screen.dart
+++ lib/screens/team_screen.dart
@@ -58,78 +58,85 @@ class _TeamScreenState extends State<TeamScreen> {
     final size = MediaQuery.of(context).size;
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          AnimatedContainer(
-            duration: 500.milliseconds,
-            padding: EdgeInsets.all(3),
-            decoration: BoxDecoration(
-                color: selected == i ? Color(0xff4285f4) : Colors.white,
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.grey)),
-            child: GestureDetector(
-              onTap: () {
-                selected = i;
-                setState(() {});
-              },
-              child: CircleAvatar(
-                radius: 48,
-                backgroundImage: AssetImage(data['image']),
-              ),
-            ),
-          ),
-          Padding(padding: EdgeInsets.only(right: 32)),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                data['name'],
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyText2
-                    .copyWith(fontSize: 16, color: Color(0xff4285F4)),
-              ),
-              Padding(padding: EdgeInsets.only(top: 16)),
-              Container(
-                // height: 40,
-                width: size.width / 1.9,
-                child: Text(
-                  data['role'],
-                  style: Theme.of(context).textTheme.bodyText2.copyWith(
-                        fontSize: 14,
-                        fontWeight: FontWeight.normal,
-                        color: Color(0xff707070),
-                      ),
+      child: Container(
+        // color: Colors.red,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            AnimatedContainer(
+              duration: 500.milliseconds,
+              padding: EdgeInsets.all(3),
+              decoration: BoxDecoration(
+                  color: selected == i ? Color(0xff4285f4) : Colors.white,
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.grey)),
+              child: GestureDetector(
+                onTap: () {
+                  selected = i;
+                  setState(() {});
+                },
+                child: CircleAvatar(
+                  radius: 48,
+                  backgroundColor: Colors.white,
+                  child: CircleAvatar(
+                    radius: 46,
+                    backgroundImage: AssetImage(data['image']),
+                  ),
                 ),
               ),
-              Padding(padding: EdgeInsets.only(top: 16)),
-              Row(
-                children: [
-                  TextButton.icon(
-                      onPressed: () {
-                        launch(data['other_url']);
-                      },
-                      icon: FaIcon(FontAwesomeIcons.link, size: 12),
-                      label: Text('')),
-                  TextButton.icon(
-                      onPressed: () {
-                        launch(data['github']);
-                      },
-                      icon: FaIcon(FontAwesomeIcons.github, size: 12),
-                      label: Text('')),
-                  TextButton.icon(
-                      onPressed: () {
-                        launch(data['linkedin']);
-                      },
-                      icon: FaIcon(FontAwesomeIcons.linkedin, size: 12),
-                      label: Text('')),
-                ],
-              ),
-            ],
-          )
-        ],
+            ),
+            Padding(padding: EdgeInsets.only(right: 32)),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  data['name'],
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText2
+                      .copyWith(fontSize: 16, color: Color(0xff4285F4)),
+                ),
+                Padding(padding: EdgeInsets.only(top: 16)),
+                Container(
+                  // height: 40,
+                  width: size.width / 1.9,
+                  child: Text(
+                    data['role'],
+                    style: Theme.of(context).textTheme.bodyText2.copyWith(
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal,
+                          color: Color(0xff707070),
+                        ),
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(top: 16)),
+                Row(
+                  children: [
+                    TextButton.icon(
+                        onPressed: () {
+                          launch(data['other_url']);
+                        },
+                        icon: FaIcon(FontAwesomeIcons.link, size: 16),
+                        label: Text('')),
+                    TextButton.icon(
+                        onPressed: () {
+                          launch(data['github']);
+                        },
+                        icon: FaIcon(FontAwesomeIcons.github, size: 16),
+                        label: Text('')),
+                    TextButton.icon(
+                        onPressed: () {
+                          launch(data['linkedin']);
+                        },
+                        icon: FaIcon(FontAwesomeIcons.linkedin, size: 16),
+                        label: Text('')),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
