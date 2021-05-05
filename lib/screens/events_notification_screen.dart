2a59ef394b33412a2ac0fb36ffe9e9fc26c52f9a
Ayush783 <ayushc45xxx@gmail.com>
2021-05-05 21:58:35 +0530

    events notification screen added

---
+++ lib/screens/events_notification_screen.dart
@@ -0,0 +1,70 @@
+import 'package:dsckiet/constants.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class EventNotificationScreen extends StatelessWidget {
+  final RemoteMessage message;
+
+  const EventNotificationScreen({Key key, this.message}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                message.notification.title,
+                style: subHeading(context),
+              ),
+              largePadding,
+              Image.network(
+                message.notification.android.imageUrl,
+              ),
+              smallPadding,
+              Text(
+                message.notification.body,
+                style: body2(context).copyWith(
+                  height: 1.33,
+                  wordSpacing: 1.3,
+                ),
+              ),
+              smallPadding,
+              Text(
+                'Timing : 1 PM | 10 April\'21',
+                style: body1(context).copyWith(fontWeight: FontWeight.bold),
+              ),
+              Spacer(),
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    launch(
+                      message.data['link'],
+                    );
+                  },
+                  child: Text('Launch'),
+                ),
+              ),
+              smallPadding,
+              Center(
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Skip >>'),
+                ),
+              ),
+              smallPadding,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/services/firebase_messaging.dart
+++ lib/services/firebase_messaging.dart
@@ -1,10 +1,10 @@
-import 'package:dsckiet/services/utility.dart';
+import 'package:dsckiet/screens/events_notification_screen.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class FirebaseNotifications {
   final _instance = FirebaseMessaging.instance;
-  final _utility = Utility();
   Future<void> subscribeTonotifications() async {
     _instance.subscribeToTopic("events");
   }
@@ -12,20 +12,33 @@ class FirebaseNotifications {
   recieveForegroundMesssage(BuildContext context) {
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       if (message != null) {
-        _utility.showAlertDialog(message, context);
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => EventNotificationScreen(
+            message: message,
+          ),
+        ));
       }
     });
   }
 
   showMessageFromTerminatedState(BuildContext context) async {
     final messagefromTerminatedstate = await _instance.getInitialMessage();
-    if (messagefromTerminatedstate != null)
-      _utility.showAlertDialog(messagefromTerminatedstate, context);
+    if (messagefromTerminatedstate != null) {
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => EventNotificationScreen(
+          message: messagefromTerminatedstate,
+        ),
+      ));
+    }
   }
 
   recieveBackgroundMessage(BuildContext context) {
     FirebaseMessaging.onMessageOpenedApp.listen((message) {
-      if (message != null) _utility.showAlertDialog(message, context);
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => EventNotificationScreen(
+          message: message,
+        ),
+      ));
     });
   }
 }
